6ce0699b3f33571079e50c1a736e44d32345e2ca
Laurent Plagne <laurent.plagne@triscale-innov.com>
2021-02-09 22:26:26 +0100

    Comments

--- src/animate2D.jl
+++ src/animate2D.jl
@@ -38,33 +38,47 @@ function update_force!(fx::AbstractArray{Float64,2},xc::AbstractArray{Float64,2}
         -xc[r1,r2])
 end
 
+"""
+    density_shift!(dc,xc,yc,ls)
+
+Approximate density variation for the 2D mass system :
+
+    dc[i,j]=δρij≃1/(Sij)-1/(4ls) where 
+
+        Sij is the surounding surface related to ij mass 
+        Sij= (xc[i+1,j]-xc[i-1,j])*(yc[i,j+1]-yc[i-1,j])
+
+        and 
+        
+        4ls is the steady surrounding surface 
+
+"""
 function density_shift!(dc,xc,yc,ls)
-    ns=size(xc,1)
-    r0,r1,r2=1:ns-2,2:ns-1,3:ns
-    ds=1/(4ls^2)
+    ns=size(xc,1) #assuming square array
+    r0,r1,r2=1:ns-2,2:ns-1,3:ns # UnitRange
+    ds=1/(4ls^2)  #steady density
     @views @. dc[r1,r1] = 1/((xc[r2,r1] - xc[r0,r1])*(yc[r1,r2] - yc[r1,r0])) - ds
 end
 
 
-
+#V is the type of the returned arrays (type are first class citizens)
 function initialize_arrays(sp,ip,ap,V)
     ls,ms,ks,ns=getvalues(sp)
     λ,shift,pos=getvalues(ip)
     δt,nδt,nδtperframe=getvalues(ap)
 
-    cxs=[steady_position(i,ls) for i ∈ 1:ns, j ∈ 1:ns]
+    #2D Array comprehensions 
+    #  -> cxs,xys,cxc,cyc are 2D arrays of size (ns,ns)
+    cxs=[steady_position(i,ls) for i ∈ 1:ns, j ∈ 1:ns] 
     cys=[steady_position(j,ls) for i ∈ 1:ns, j ∈ 1:ns]
-
-    xaxis=[steady_position(i,ls) for i ∈ 1:ns]
-
     cxc=[initial_position(i,j,ls,λ,shift,pos,pos)[1] for i ∈ 1:ns, j ∈ 1:ns]
     cyc=[initial_position(i,j,ls,λ,shift,pos,pos)[2] for i ∈ 1:ns, j ∈ 1:ns]
 
+    #Broadcast of V Ctor equivalent to xs=V(cxs);ys=V(cys);xc=V(cxc);yc=V(cyc)
     xs,ys,xc,yc=V.((cxs,cys,cxc,cyc))
-
+    #Assume zero initial velocities : xp(revious)=xc(urrent)
     fx,xt,xp=zero(xc),zero(xc),copy(xc)
     fy,yt,yp=zero(yc),zero(yc),copy(yc)
-
 
     xs,xc,xp,xt,fx,ys,yc,yp,yt,fy
 end
@@ -72,13 +86,13 @@ end
 
 
 function display_perf(td,xc,nδt)
-    nflops=20
-    nRW=12
-    T=eltype(xc) # Float64
+    nflops=20 # Manual count floating point ops
+    nRW=12    # Manual count Array Reads and Writes
+    T=eltype(xc) # i.e. Float64
     floatsize_inbytes=sizeof(T) #Float64 -> 8 Bytes
     nbytes=nRW*floatsize_inbytes # Float64 -> 96 Bytes
     
-    mupns=length(xc)*nδt/(td*1.e9) # mass pos update per ns
+    mupns=length(xc)*nδt/(td*1.e9) # mass pos updates per ns
     println("$(round((mupns*nflops),sigdigits=3))\t GFLOPS")
     println("$(round((mupns*nbytes),sigdigits=3))\t GB/s")
 end
@@ -88,24 +102,26 @@ function animate_spring2D(sp,ip,ap,V=Array{Float64,2})
     δt,nδt,nδtperframe=getvalues(ap)
     xs,xc,xp,xt,fx,ys,yc,yp,yt,fy=initialize_arrays(sp,ip,ap,V)
 
-    dc=zero(xc)
-    cdc=Array(xc)
+    dc=zero(xc)    # Displacement (type V)
+    cdc=Array(xc)  # Displacement (type Array{Float64,2})
 
-    xaxis=Array(xs[:,1])
-    yaxis=Array(ys[1,:])
-    nf=nδt÷nδtperframe
+    xaxis=Array(xs[:,1]) #1D Array
+    yaxis=Array(ys[1,:]) #1D Array
+    nf=nδt÷nδtperframe # We assume a null remainder
 
-    t=0.0
-    tdynamic=0.0
+    t=0.0 # Simulation time 
+    tdynamic=0.0 # Computing time for the dynamic
     anim = @animate for i ∈ 1:nf
+
         tdynamic+= @elapsed CUDA.@sync begin
             xc,xp,xt=advance_nδtpf(xc,xp,xt,fx,sp,ap)
             yc,yp,yt=advance_nδtpf(yc,yp,yt,fy,sp,ap)
         end
-        @. dc = sqrt((xc - xs)^2+(yc - ys)^2)
-        @. cdc = dc 
+        @. dc = sqrt((xc - xs)^2+(yc - ys)^2) #displacement norm
+        @. cdc = dc  # Copy dc (type V) to cdc (type Array{Float64,2})
+
         t+=nδtperframe*δt
-        contour(xaxis,yaxis,cdc,clims=(0,ip.shift/2),
+        contour(xaxis,yaxis,cdc,clims=(0,ip.shift/2), #level lines graph
             thickness_scaling = 1.4,
             size=(600,600),
             framestyle = :box,
@@ -115,7 +131,7 @@ function animate_spring2D(sp,ip,ap,V=Array{Float64,2})
             title=L"\sqrt{(x_c-x_s)^2+(y_c-y_s)^2}",aspect_ratio=:equal)
     end
 
-    display_perf(tdynamic,xc,nδt)
+    display_perf(tdynamic,xc,nδt) #Display GFLOPS and GB/s
 
-    gif(anim,"toto.gif",fps=15)
+    gif(anim,"toto.gif",fps=15) # save the animation 
 end
